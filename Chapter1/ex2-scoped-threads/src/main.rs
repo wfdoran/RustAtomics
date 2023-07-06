4fcb120ae5a2992ad448fb794dbe0c5b67d00868
William Doran <wfdoran4@yahoo.com>
2023-07-05 17:31:57 -0700

    Chapter 1, Exercise 2: Scoped Threads.

---
+++ Chapter1/ex2-scoped-threads/src/main.rs
@@ -0,0 +1,24 @@
+use std::thread;
+
+fn main() {
+    let numbers = vec![1,2,3];
+
+    thread::scope(|s| {
+        s.spawn( || {
+            println!("length: {}", numbers.len());
+        });
+
+        s.spawn(|| {
+            for n in &numbers {
+                println!("{n}");
+            }
+
+        });
+
+        /*  
+        s.spawn(|| {
+            numbers.push(4);
+        });
+        */
+    });
+}
