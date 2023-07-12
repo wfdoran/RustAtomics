0455c82a42a4f6060add7da0ce424e14f159599b
William Doran <wfdoran4@yahoo.com>
2023-07-12 16:16:59 -0700

    Chapter 1, Example 7, added RwLock example.

--- Chapter1/ex7-mutexes/src/main.rs
+++ Chapter1/ex7-mutexes/src/main.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::sync::Mutex;
+use std::sync::RwLock;
 use std::time::Duration;
 
 fn main() {
@@ -20,4 +21,58 @@ fn main() {
     println!();
 
     println!("{}", n.into_inner().unwrap());
+
+    let a = Mutex::new(Vec::<i32>::new());
+
+    thread::scope(|s| {
+        for _ in 0..2 {
+            s.spawn(|| {
+                for i in 0..10 {
+                    a.lock().unwrap().push(i);
+                }
+
+            });
+        }
+    });
+
+    let a = a.into_inner().unwrap();
+    println!("{:?}", a);
+
+
+    let a = RwLock::new(0);
+    let a_max = 10;
+
+    thread::scope(|s| 
+    {
+        s.spawn(|| {
+            let mut val = 0;
+            while val < a_max {
+                let guard = a.read().unwrap();
+                let cur = *guard;
+                if cur != val {
+                    println!("A {cur}");
+                    val =  cur;
+                }
+            }
+        });
+        s.spawn(|| {
+            let mut val = 0;
+            while val < a_max {
+                let guard = a.read().unwrap();
+                let cur = *guard;
+                if cur != val {
+                    println!("B {cur}");
+                    val =  cur;
+                }
+            }
+        });
+
+        s.spawn(|| {
+            for val in 1..=a_max {
+                let mut guard = a.write().unwrap();
+                *guard = val;
+            }
+        });
+
+    });
 }
