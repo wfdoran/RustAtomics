d8885348b9056b58cf505f2c87cf3cacee613d68
William Doran <wfdoran4@yahoo.com>
2023-07-11 18:50:03 -0700

    Chapter 1, Example 7, Mutexes

---
+++ Chapter1/ex7-mutexes/src/main.rs
@@ -0,0 +1,23 @@
+use std::thread;
+use std::sync::Mutex;
+use std::time::Duration;
+
+fn main() {
+    let n = Mutex::new(0);
+
+    thread::scope(|s| {
+        for _ in 0..10 {
+            s.spawn(|| {
+                let mut guard = n.lock().unwrap();
+                for _ in 0..100 {
+                    *guard += 1;
+                }
+                drop(guard);
+                thread::sleep(Duration::from_secs(1));
+            });
+        }
+    });
+    println!();
+
+    println!("{}", n.into_inner().unwrap());
+}
