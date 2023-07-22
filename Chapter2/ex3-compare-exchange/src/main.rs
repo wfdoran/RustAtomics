e0d4adf0b418311541aaa2f6416fbb96aa743f38
William Doran <wfdoran4@yahoo.com>
2023-07-22 12:58:44 -0700

    Chapter 2, Example 3, id allocation using comp/exch.

--- Chapter2/ex3-compare-exchange/src/main.rs
+++ Chapter2/ex3-compare-exchange/src/main.rs
@@ -1,6 +1,7 @@
 use std::thread;
 use std::sync::atomic::AtomicU32;
 use std::sync::atomic::Ordering::Relaxed;
+use std::time;
 
 fn increment(a: &AtomicU32) {
     let mut current = a.load(Relaxed);
@@ -36,7 +37,60 @@ fn increment_example() {
     println!("{:?}", a);
 }
 
+fn allocate_new_id() -> u32 {
+    static NEXT_ID: AtomicU32 = AtomicU32::new(0);
+
+    let mut id = NEXT_ID.load(Relaxed);
+    loop {
+        assert!(id < 1000, "too many IDs");
+        loop {
+            match NEXT_ID.compare_exchange_weak(id, id+1, Relaxed, Relaxed) {
+                Ok(_) => return id,
+                Err(v) => id = v,
+            }
+        }
+    }
+}
+
+fn allocate_new_id2() -> u32 {
+    static NEXT_ID: AtomicU32 = AtomicU32::new(0);
+    NEXT_ID.fetch_update(Relaxed, Relaxed, |n| n.checked_add(1)).expect("too many IDs!")
+}
+
+fn allocation_example() {
+    let num_threads = 5;
+    let ids_per_thread = 20;
+
+    thread::scope( |s| {
+        for _ in 0..num_threads {
+            s.spawn(|| {
+                for _ in 0..ids_per_thread {
+                    let id = allocate_new_id();
+                    print!("{id} ");
+                    thread::sleep(time::Duration::from_millis(100));
+                }
+            });
+        }
+    });
+    println!();
+
+    thread::scope( |s| {
+        for _ in 0..num_threads {
+            s.spawn(|| {
+                for _ in 0..ids_per_thread {
+                    let id = allocate_new_id2();
+                    print!("{id} ");
+                    thread::sleep(time::Duration::from_millis(100));
+                }
+            });
+        }
+    });
+    println!();
+   
+}
+
 
 fn main() {
     increment_example();
+    allocation_example();
 }
